4f0fa019f3314f7acc70f9ceb6b0f72fd845024a
Tobias Heine <spam@tobiasheine.eu>
2015-01-11 17:50:31 +0100

    wip

--- mobile/src/main/java/eu/tobiasheine/bitcoinwatcher/widget/BitcoinWatcherWidgetProvider.java
+++ mobile/src/main/java/eu/tobiasheine/bitcoinwatcher/widget/BitcoinWatcherWidgetProvider.java
@@ -1,18 +1,29 @@
 package eu.tobiasheine.bitcoinwatcher.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.view.Gravity;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 import eu.tobiasheine.bitcoinwatcher.R;
-import eu.tobiasheine.bitcoinwatcher.settings.Settings;
 import eu.tobiasheine.bitcoinwatcher.dao.storage.Storage;
+import eu.tobiasheine.bitcoinwatcher.price_sync.SyncService;
+import eu.tobiasheine.bitcoinwatcher.price_sync.Synchronization;
+import eu.tobiasheine.bitcoinwatcher.settings.Settings;
 import eu.tobiasheine.bitcoinwatcher.widget.ui.BitcoinWatcherViewModelFactory;
 import eu.tobiasheine.bitcoinwatcher.widget.ui.BitcoinWatcherWidgetViewModel;
 
 public class BitcoinWatcherWidgetProvider extends AppWidgetProvider {
+
+    private PendingIntent startSyncIntent;
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        startSyncIntent = PendingIntent.getService(context, 0, new Intent(context,SyncService.class), 0);
+    }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -28,11 +39,11 @@ public class BitcoinWatcherWidgetProvider extends AppWidgetProvider {
 
         final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.bitcoin_watcher_appwidget);
 
-        //views.setInt(R.id.tvCurrentPrice,"setGravity", Gravity.END);
-        //views.setInt(R.id.tvUpdatedAt,"setGravity", Gravity.END);
-
         views.setTextViewText(R.id.tvCurrentPrice, currentPrice);
         views.setTextViewText(R.id.tvUpdatedAt, updatedAt);
+        views.setTextViewText(R.id.tvWidgetTitle, "Bitcoin Watcher");
+
+        views.setOnClickPendingIntent(R.id.btSync, startSyncIntent);
 
         for (int appWidgetId : appWidgetIds) {
             appWidgetManager.updateAppWidget(appWidgetId, views);
